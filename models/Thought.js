91d2c1c545f9e5274e726f9b699c2034bb4009f1
jbradley84 <utbradley84@yahoo.com>
2022-08-28 11:26:25 -0500

    added dateFormat.js and dateFormat getters to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,56 +1,64 @@
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const ReactionSchema = new Schema({
-    // set custom id to avoid confusion with parent thought _id
-    reactionId: {
-        type: Schema.Type.ObjectId,
-        default: () => new Types.ObjectId()
-    },
-    reactionBody: {
-        type: String,
-        required: true,
-        maxLength: 280
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-});
+   // set custom id to avoid confusion with parent thought _id
+   reactionId: {
+      type: Schema.Type.ObjectId,
+      default: () => new Types.ObjectId()
+   },
+   reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
+   },
+   username: {
+      type: String,
+      required: true
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+   }
+},
+   {
+      toJSON: {
+         getters: true
+      },
+   });
 
 const ThoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minLength: 1,
-            maxLength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [ReactionSchema]
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
+   {
+      thoughtText: {
+         type: String,
+         required: true,
+         minLength: 1,
+         maxLength: 280
+      },
+      createdAt: {
+         type: Date,
+         default: Date.now,
+         get: createdAtVal => dateFormat(createdAtVal)
+      },
+      username: {
+         type: String,
+         required: true
+      },
+      reactions: [ReactionSchema]
+   },
+   {
+      toJSON: {
+         virtuals: true,
+         getters: true
+      },
+      id: false
+   }
 );
 
 // get total count of reactions 
-ThoughtSchema.virtal('reactionCount').get(function() {
-    return this.reactions.length;
+ThoughtSchema.virtal('reactionCount').get(function () {
+   return this.reactions.length;
 });
 
 const Thought = model('Thought', ThoughtSchema);
