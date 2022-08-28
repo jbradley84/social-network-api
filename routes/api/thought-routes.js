686025dd56a25e60ed1e44d761958e26b5eb9ed4
jbradley84 <utbradley84@yahoo.com>
2022-08-28 11:12:55 -0500

    exported thought routes through thought-routes.js

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const {
+   getAllThought,
+   getThoughtById,
+   createThought,
+   updateThought,
+   addReaction,
+   removeThought,
+   removeReaction
+} = require('../../controllers/thought-controller');
+
+// set up GET all and POST at /api/thoughts
+router.route('/')
+.get(getAllThought)
+.post(createThought);
+
+// set up GET one, PUT, and DELETE at /api/thoughts/:id
+router.route('/:id')
+.get(getThoughtById)
+.put(updateThought)
+.delete(removeThought);
+
+// set up POST at /api/thoughts/:id/reactions
+router.route('/:id/reactions')
+.post(addReaction);
+
+// set up DELETE at /api/thoughts/:id/reactions/:reactionId
+router.route('/:id/reactions/:reactiionId')
+.delete(removeReaction);
+
+module.exports = router;
